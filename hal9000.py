4bd3406ba1e44856430531aee86969c2b761e96f
Juergen Pabel <juergen@pabel.net>
2024-05-22 11:50:51 +0200

    Initial implementation (publishes an mqtt message upon instantiation)

---
+++ hal9000.py
@@ -0,0 +1,29 @@
+import time
+
+from threading import Thread
+import paho.mqtt.publish as mqtt_publish
+
+from kalliope.core import SignalModule, MissingParameter
+
+from kalliope.core.ConfigurationManager import BrainLoader
+from kalliope.core.SynapseLauncher import SynapseLauncher
+from kalliope.core import Utils
+
+
+class Hal9000(SignalModule):
+	def __init__(self, **kwargs):
+		super(Hal9000, self).__init__(**kwargs)
+		for synapse in list(super(Hal9000, self).get_list_synapse()):
+			for signal in synapse.signals:
+				if signal.name == 'hal9000':
+					for neuron in synapse.neurons:
+						if neuron.name == 'mqtt_publisher':
+							mqtt_publish.single(neuron.parameters['topic'], "starting", hostname=neuron.parameters['broker_ip'])
+
+	@staticmethod
+	def check_parameters(parameters):
+		return True
+
+	def on_notification_received(self, notification=None, payload=None):
+		pass
+
