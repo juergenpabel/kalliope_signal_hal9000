05727839b31b68da8033f9cae3fbfde0a7e69174
Juergen Pabel <juergen@pabel.net>
2024-06-13 10:48:47 +0200

    made the mqtt connection persistent (for the lifetime of the signal handler)

--- hal9000.py
+++ hal9000.py
@@ -1,7 +1,7 @@
 from time import sleep
 from threading import Thread
+from paho.mqtt import client as mqtt_client
 import logging
-import paho.mqtt.publish as mqtt_publish
 
 from kalliope.core import SignalModule, MissingParameter
 from kalliope.core.Cortex import Cortex
@@ -16,6 +16,7 @@ class Hal9000(SignalModule, Thread):
 		Thread.__init__(self, name=Hal9000)
 		self.mqtt_broker_ip = '127.0.0.1'
 		self.mqtt_broker_port = 1883
+		self.mqtt_client_id = 'kalliope-signal-hal9000'
 		self.mqtt_topic = 'hal9000/event/kalliope/interface/state'
 		for synapse in list(super(Hal9000, self).get_list_synapse()):
 			for signal in synapse.signals:
@@ -24,16 +25,21 @@ class Hal9000(SignalModule, Thread):
 						if neuron.name == 'mqtt_publisher':
 							self.mqtt_broker_ip = neuron.parameters.get('broker_ip', self.mqtt_broker_ip)
 							self.mqtt_broker_port = neuron.parameters.get('port', self.mqtt_broker_port)
+							self.mqtt_client_id = neuron.parameters.get('client_id', self.mqtt_client_id)
 							self.mqtt_topic = neuron.parameters.get('topic', self.mqtt_topic)
 		Cortex.save('kalliope_status', 'starting')
 
 	def run(self):
 		Utils.print_info('[Hal9000] Starting thread')
 		try:
+			mqtt = mqtt_client.Client(self.mqtt_client_id)
+			mqtt.connect(self.mqtt_broker_ip, self.mqtt_broker_port)
 			while Cortex.get_from_key('kalliope_status') == 'starting':
-				mqtt_publish.single(self.mqtt_topic, 'starting', hostname=self.mqtt_broker_ip, port=self.mqtt_broker_port,
-				                    client_id='kalliope-signal-hal9000')
-				sleep(1)
+				mqtt.publish(self.mqtt_topic, 'starting')
+				mqtt.loop(timeout=0.5)
+				sleep(0.5)
+			mqtt.disconnect()
+			mqtt.loop_forever()
 		except BaseException as e:
 			logger.info(f"[signal:hal9000] {e}")
 		Utils.print_info('[Hal9000] Ending thread')
