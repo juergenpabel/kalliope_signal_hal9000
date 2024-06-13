e637f3e7344dd5292661a79335495612162f020a
Juergen Pabel <juergen@pabel.net>
2024-06-13 11:06:08 +0200

    moved mqtt configuration into signal (instead of looking into mqtt_publisher config)

--- hal9000.py
+++ hal9000.py
@@ -16,17 +16,15 @@ class Hal9000(SignalModule, Thread):
 		Thread.__init__(self, name=Hal9000)
 		self.mqtt_broker_ip = '127.0.0.1'
 		self.mqtt_broker_port = 1883
-		self.mqtt_client_id = 'kalliope-signal-hal9000'
+		self.mqtt_client_id = 'kalliope:signal:hal9000'
 		self.mqtt_topic = 'hal9000/event/kalliope/interface/state'
 		for synapse in list(super(Hal9000, self).get_list_synapse()):
 			for signal in synapse.signals:
-				if signal.name == 'hal9000':
-					for neuron in synapse.neurons:
-						if neuron.name == 'mqtt_publisher':
-							self.mqtt_broker_ip = neuron.parameters.get('broker_ip', self.mqtt_broker_ip)
-							self.mqtt_broker_port = neuron.parameters.get('port', self.mqtt_broker_port)
-							self.mqtt_client_id = neuron.parameters.get('client_id', self.mqtt_client_id)
-							self.mqtt_topic = neuron.parameters.get('topic', self.mqtt_topic)
+				if signal.name == 'hal9000' and signal.parameters is not None:
+					self.mqtt_broker_ip = signal.parameters.get('broker_ip', self.mqtt_broker_ip)
+					self.mqtt_broker_port = signal.parameters.get('port', self.mqtt_broker_port)
+					self.mqtt_client_id = signal.parameters.get('client_id', self.mqtt_client_id)
+					self.mqtt_topic = signal.parameters.get('topic', self.mqtt_topic)
 		Cortex.save('kalliope_status', 'starting')
 
 	def run(self):
